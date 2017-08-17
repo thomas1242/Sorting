4583ec11015a34935e2d8f500da600c460481ca8
thomas1242 <thomaspalmer121@gmail.com>
2017-08-17 12:57:04 -0400

    interpolation

--- src/Interpolation.java
+++ src/Interpolation.java
@@ -5,23 +5,20 @@ public class Interpolation {
     public static Color[] getColors(int start, int end, int length) {
         Color[] colors = new Color[length];
 
-        int intARGB;                            // integer to hold synthesized color values
-        int value = start;
-        double value_R = (value >> 16) & 0xFF;
-        double value_G = (value >> 8 ) & 0xFF;
-        double value_B = (value      ) & 0xFF;
+        double value_R = (start >> 16) & 0xFF;
+        double value_G = (start >> 8 ) & 0xFF;
+        double value_B = (start      ) & 0xFF;
 
         double[] deltas = getDeltas( start, end, colors.length - 1 );
         colors[0] = new Color(start);
         colors[colors.length - 1] = new Color(end);
 
-        // fill with interpolated Colors
+        // fill 1D array with interpolated Colors
         for (int i = 1; i < colors.length - 1; i++) {
             value_R += deltas[0];
             value_G += deltas[1];
-            value_B += deltas[2];
-
-            intARGB = (0xFF000000) | ((int)value_R << 16) | ((int)value_G << 8) | (int)value_B;
+            value_B += deltas[2];                    
+            int intARGB = (0xFF000000) | ((int)value_R << 16) | ((int)value_G << 8) | (int)value_B;
             colors[i] = new Color(intARGB);
         }
 
@@ -30,8 +27,8 @@ public class Interpolation {
 
     private static double[] getDeltas(int start, int end, int n) {
         double start_R, start_G, start_B,
-                end_R,   end_G,   end_B,
-                delta_R, delta_G, delta_B;
+                 end_R,   end_G,   end_B,
+               delta_R, delta_G, delta_B;
 
         end_R = (end >> 16) & 0xFF;
         end_G = (end >> 8 ) & 0xFF;
@@ -41,12 +38,12 @@ public class Interpolation {
         start_G = (start >> 8 ) & 0xFF;
         start_B = (start      ) & 0xFF;
 
-        delta_R = (end_R - start_R) / n;        // change per color channel
+        delta_R = (end_R - start_R) / n;        // color channel change per step
         delta_G = (end_G - start_G) / n;
         delta_B = (end_B - start_B) / n;
 
         double[] deltas = { delta_R, delta_G, delta_B };
         return deltas;
     }
-
+    
 }
