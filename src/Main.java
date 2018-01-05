4426514d84f38606924964314bed47fb347e4f62
thomas1242 <thomaspalmer121@gmail.com>
2018-01-04 19:08:17 -0500

    style

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,19 @@ import java.awt.*;
 
 public class Main {
 
-    private static final int WIDTH  = (int)java.awt.Toolkit.getDefaultToolkit().getScreenSize().getWidth();   // full screen
-    private static final int HEIGHT = (int)java.awt.Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+    private static final int WIDTH  = (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+    private static final int HEIGHT = (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight();
 
-    public static void main( String[] args ) {
-        SwingUtilities.invokeLater(() -> createAndShowGUI());
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater( () -> createAndShowGUI() );
     }
 
     private static void createAndShowGUI() {
         JFrame frame = new JFrame();
         frame.setSize(WIDTH, HEIGHT);
         frame.add(new ImagePanel(WIDTH, HEIGHT), BorderLayout.CENTER);
-        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-        frame.setVisible( true );
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
-
+    
 }
